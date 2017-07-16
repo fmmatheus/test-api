b7823ecd3d80392df1608701b7afe0af20e7d1c7
matheus.moreira <fmmatheus@gmail.com>
2017-07-16 19:56:46 -0300

    Add e-mail validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   require 'correios-cep'
 
+  before_save { email.downcase! }
+
   # validations
-  validates_presence_of :name, :email, :address
-  validate              :set_address
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates_presence_of :name
+  validates_presence_of :email, format: { with: VALID_EMAIL_REGEX }
+  validates_presence_of :address, :set_address
 
   def set_address
     self.address = Correios::CEP::AddressFinder.get(self.address)
